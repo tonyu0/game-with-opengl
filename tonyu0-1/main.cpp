1fc02226ba2e0b91aea606536152ea51993e3f56
nakagawa <nakagawa1011@tabitora.com>
2019-03-19 13:32:31 +0900

    Initial Commit

---
+++ tonyu0-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tonyu0-1
+//
+//  Created by Nakagawa on 2019/03/19.
+//  Copyright © 2019 Nakagawa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
