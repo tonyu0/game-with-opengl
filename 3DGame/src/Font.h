e31e9381c473b0159a32c471cf1e5ddcba52fcf9
tonyu0 <tonyu.grapefruit@gmail.com>
2021-10-12 14:00:17 +0900

    Update to x64 app

--- 3DGame/src/Font.h
+++ 3DGame/src/Font.h
@@ -7,20 +7,20 @@
 
 class Font {
 public:
-  Font(class Game *game);
-  ~Font();
+	Font(class Game* game);
+	~Font();
 
-  // Load/unload from a file
-  bool Load(const std::string &fileName);
-  void Unload();
+	// Load/unload from a file
+	bool Load(const std::string& fileName);
+	void Unload();
 
-  // Given string and this font, draw to a texture
-  class Texture *RenderText(const std::string &textKey,
-                            const Vector3 &color = Color::White,
-                            int pointSize = 30);
+	// Given string and this font, draw to a texture
+	class Texture* RenderText(const std::string& textKey,
+		const Vector3& color = Color::White,
+		int pointSize = 30);
 
 private:
-  // Map of point sizes to font data
-  std::unordered_map<int, TTF_Font *> mFontData;
-  class Game *mGame;
+	// Map of point sizes to font data
+	std::unordered_map<int, TTF_Font*> mFontData;
+	class Game* mGame;
 };
--- 3DGame/src/Texture.cpp
+++ 3DGame/src/Texture.cpp
@@ -1,93 +1,92 @@
 #include "Texture.h"
 #include <GL/glew.h>
 #include <SDL/SDL.h>
-#include <SOIL/SOIL.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
 Texture::Texture() : mTextureID(0), mWidth(0), mHeight(0) {}
 
 Texture::~Texture() {}
 
-bool Texture::Load(const std::string &fileName) {
-  mFileName = fileName;
-  int channels = 0;
+bool Texture::Load(const std::string& fileName) {
+	mFileName = fileName;
+	int channels = 0;
+	unsigned char* image = stbi_load(fileName.c_str(), &mWidth, &mHeight, &channels, 0);
 
-  unsigned char *image = SOIL_load_image(fileName.c_str(), &mWidth, &mHeight,
-                                         &channels, SOIL_LOAD_AUTO);
+	if (image == nullptr) {
+		SDL_Log("SOIL failed to load image %s: %s", fileName.c_str());
+		return false;
+	}
+	SDL_Log("Texture Name: %s, Size: %d x %d, Channels: %d", fileName.c_str(), mHeight, mWidth, channels);
 
-  if (image == nullptr) {
-    SDL_Log("SOIL failed to load image %s: %s", fileName.c_str(),
-            SOIL_last_result());
-    return false;
-  }
+	int format = GL_RGB;
+	if (channels == 4) {
+		format = GL_RGBA;
+	}
+	// Search unused texture ID.
+	glGenTextures(1, &mTextureID);
+	// Upload texture to VRAM, make OpenGL texture object.
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
 
-  int format = GL_RGB;
-  if (channels == 4) {
-    format = GL_RGBA;
-  }
-  // Search unused texture ID.
-  glGenTextures(1, &mTextureID);
-  // Upload texture to VRAM, make OpenGL texture object.
-  glBindTexture(GL_TEXTURE_2D, mTextureID);
+	// copy raw image data.
+	glTexImage2D(GL_TEXTURE_2D, 0, format, mWidth, mHeight, 0, format,
+		GL_UNSIGNED_BYTE, image);
+	// Free SOIL image data from memory.
+	stbi_image_free(image);
 
-  // copy raw image data.
-  glTexImage2D(GL_TEXTURE_2D, 0, format, mWidth, mHeight, 0, format,
-               GL_UNSIGNED_BYTE, image);
-  // Free SOIL image data from memory.
-  SOIL_free_image_data(image);
+	// Generate mipmaps for texture
+	glGenerateMipmap(GL_TEXTURE_2D);
+	// Enable linear filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+		GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  // Generate mipmaps for texture
-  glGenerateMipmap(GL_TEXTURE_2D);
-  // Enable linear filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                  GL_LINEAR_MIPMAP_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Enable aniostropic filtering, if supported
+	if (GLEW_EXT_texture_filter_anisotropic) {
+		// Get the maximum anisotropy value
+		GLfloat largest;
+		glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &largest);
+		// Enable it
+		glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, largest);
+	}
 
-  // Enable aniostropic filtering, if supported
-  if (GLEW_EXT_texture_filter_anisotropic) {
-    // Get the maximum anisotropy value
-    GLfloat largest;
-    glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &largest);
-    // Enable it
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, largest);
-  }
-
-  return true;
+	return true;
 }
 
 void Texture::Unload() { glDeleteTextures(1, &mTextureID); }
 
-void Texture::CreateFromSurface(SDL_Surface *surface) {
-  mWidth = surface->w;
-  mHeight = surface->h;
+void Texture::CreateFromSurface(SDL_Surface* surface) {
+	mWidth = surface->w;
+	mHeight = surface->h;
 
-  // Generate a GL texture
-  glGenTextures(1, &mTextureID);
-  glBindTexture(GL_TEXTURE_2D, mTextureID);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, GL_BGRA,
-               GL_UNSIGNED_BYTE, surface->pixels);
+	// Generate a GL texture
+	glGenTextures(1, &mTextureID);
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mWidth, mHeight, 0, GL_BGRA,
+		GL_UNSIGNED_BYTE, surface->pixels);
 
-  // Use linear filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Use linear filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void Texture::CreateForRendering(int width, int height, unsigned int format) {
-  mWidth = width;
-  mHeight = height;
-  // Create the texture id
-  glGenTextures(1, &mTextureID);
-  glBindTexture(GL_TEXTURE_2D, mTextureID);
-  // Set the image width/height with null initial data
-  glTexImage2D(GL_TEXTURE_2D, 0, format, mWidth, mHeight, 0, GL_RGB, GL_FLOAT,
-               nullptr);
+	mWidth = width;
+	mHeight = height;
+	// Create the texture id
+	glGenTextures(1, &mTextureID);
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
+	// Set the image width/height with null initial data
+	glTexImage2D(GL_TEXTURE_2D, 0, format, mWidth, mHeight, 0, GL_RGB, GL_FLOAT,
+		nullptr);
 
-  // For a texture we'll render to, just use nearest neighbor
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	// For a texture we'll render to, just use nearest neighbor
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 }
 
 void Texture::SetActive(int index) {
-  // ここをGL_TEXTUREに設定してたせいでテクスチャが１種類しか読めない状態になってた
-  glActiveTexture(GL_TEXTURE0 + index);
-  glBindTexture(GL_TEXTURE_2D, mTextureID);
+	// ここをGL_TEXTUREに設定してたせいでテクスチャが１種類しか読めない状態になってた
+	glActiveTexture(GL_TEXTURE0 + index);
+	glBindTexture(GL_TEXTURE_2D, mTextureID);
 }
